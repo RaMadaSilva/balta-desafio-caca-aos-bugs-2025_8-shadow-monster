120c85353eb0020f485580a71f4e04360b54a58a
RaMadaSilva <raul01mateia@gmail.com>
2025-11-01 21:23:23 +0100

    Chenge_CI/CD-To-Render

--- src/BugStore.Api/Program.cs
+++ src/BugStore.Api/Program.cs
@@ -8,18 +8,18 @@ builder.Services.AddSwaggerDoc();
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddDependencyInjection();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(0);
+});
+
+builder.WebHost
+    .UseSetting(WebHostDefaults.ServerUrlsKey, "http://*:" + Environment.GetEnvironmentVariable("PORT"));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
-
 
 app.MapEndpoints();
 
